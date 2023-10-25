8f93cad0d3dd19d9c50bec62e2df650a7b48aed9
XatAlcantara <143433826+XatAlcantara@users.noreply.github.com>
2023-10-25 12:00:49 -0600

    Comentario

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -8,20 +8,3 @@ export const anotherExample = () => {
 };
 
 //Dentro de dataFuntions irán todas las funciones de como filtrar, ordenar, etc
-
-function clearFilters() {
-  document.getElementById('escuderia').value = '';
-  document.getElementById('podiums-win').value = '';
-  document.getElementById('alive-filter').checked = false;
-  document.getElementById('dead-filter').checked = false;
-  document.getElementById('sort-order').value = 'asc'; // Restaura el selector de orden alfabético
-  document.getElementById('sort-order-two').value = 'most-championships'; // Restaura el selector de campeonatos
-
-  renderItems(data); // Muestra todos los pilotos sin filtros ni ordenamientos
-}
-
-// Agrega un evento para el botón "Limpiar"
-const clearFiltersButton = document.getElementById('clear-filters');
-clearFiltersButton.addEventListener('click', clearFilters);
-
-//tenemos que poner los filtros 
--- src/view.js
+++ src/view.js
@@ -33,4 +33,6 @@ export const renderItems = (data) => {
 
 
 
+
+
  

08766e3393b0b99b01a58f3905ccb7485b37052e
XatAlcantara <143433826+XatAlcantara@users.noreply.github.com>
2023-10-26 17:18:06 -0600

    Se agregaron cambios a datafuntions.js

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,10 +1,23 @@
-// Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return [];
-};
-
 //Dentro de dataFuntions irán todas las funciones de como filtrar, ordenar, etc
+import data from './data/dataset.js';
+
+
+export function filterData (data, filterBy ,value){
+
+  if (!Array.isArray(data) || typeof filterBy !==`string`) {
+    return[];
+  }
+  
+  const filteredData = data.filter(item => {
+    if (item.hasOwnProperty (filterBy)){
+      return item[filterBy]=== value;
+    }
+    return false;
+});
+    return filteredData;
+
+}
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
-import { example } from './dataFunctions.js';
+import {filterData} from './dataFunctions.js';
 import { renderItems } from './view.js';
-
 import data from './data/dataset.js';
 
 const arregloPilotos = data;
 renderItems(arregloPilotos);
 
 // Aquí deberemos llamar a las funciones incluidas en dataFuntions.js
+
+const selectFilter = document.querySelector('#select-filter'); //Llamando al elemento del dom
+selectFilter.addEventListener('change', function () {
+    const selected = selectFilter.value; // Obtiene el valor seleccionado
+    //const filteredData = filterData(data, `team`, result);
+
+    renderItems(filteredData)
+}); 
+
+//const teamFilter = document.querySelector('#select-filter'); //Llamando al elemento del dom
+//selectElement.addEventListener('change', (event) => {
+  //  const result = event.target.value;
+  //  const filteredData = filterData(data, `escuderia`, result);
+
+    // renderItems(filteredData)
+    //}); 
+

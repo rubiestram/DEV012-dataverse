6192e9c88a249ce78a3cec4f4eeae1c463029f6a
Rubi <rubiestram@gmail.com>
2023-10-30 13:40:39 -0600

    Agregue cambios a datafuntions y main

--- src/main.js
+++ src/main.js
@@ -15,18 +15,14 @@ containerRoot.appendChild(renderItems(arrayPilots));
 const filter = document.getElementById("select-filter");
 filter.addEventListener('change', function (event) {
     arrayPilots = filterData(arrayPilots, "team", event.target.value);
-    console.log(event.target.value);
-    console.log(arrayPilots);
     containerRoot.innerHTML = "";
     containerRoot.appendChild(renderItems(arrayPilots));
 });
 
-// DOM ordenar
+// DOM Order
 const order = document.getElementById("sort-order");
 order.addEventListener('change', function (event) {
-    const sortOrder = event.target.value; // Obtén el valor seleccionado ('asc' o 'desc') del select.
-    // Aquí llamamos a la función sortData para ordenar el arreglo de pilotos (arrayPilots).
-    arrayPilots = sortData(arrayPilots, 'nombre', event.target.value);
+    arrayPilots = sortData(arrayPilots, "name", event.target.value);
     containerRoot.innerHTML = "";
     containerRoot.appendChild(renderItems(arrayPilots));
 });

835779602a6ba8f18d2b57b6d958c5deb6c2045d
XatAlcantara <143433826+XatAlcantara@users.noreply.github.com>
2023-10-31 08:02:15 -0600

    cambios css al menu

--- src/main.js
+++ src/main.js
@@ -1,42 +1,60 @@
 import { filterData, sortData, computeStats } from './dataFunctions.js';
 import { renderItems } from './view.js';
 import data from './data/dataset.js';
- 
+
 let arrayPilots = data;
 let sortOption = null; // Para mantener el estado de la opción de ordenamiento
 
 // HTML dinámico
-const containerRoot  = document.querySelector("#root");
+const containerRoot = document.querySelector("#root");
 containerRoot.appendChild(renderItems(arrayPilots));
 
 // DOM filtrar
 const filter = document.getElementById("select-filter");
 filter.addEventListener('change', function (event) {
-    const selectedValue = event.target.value;
-    arrayPilots = filterData(data, "team", selectedValue);
-    // Ordenar la data filtrada, si es necesario
-    if (sortOption) {
-        arrayPilots = sortData(arrayPilots, sortOption);
-    }
-    containerRoot.innerHTML = "";
-    containerRoot.appendChild(renderItems(arrayPilots));
+  const selectedValue = event.target.value;
+  arrayPilots = filterData(data, "team", selectedValue);
+  // Ordenar la data filtrada, si es necesario
+  if (sortOption) {
+    arrayPilots = sortData(arrayPilots, sortOption);
+  }
+  containerRoot.innerHTML = "";
+  containerRoot.appendChild(renderItems(arrayPilots));
 });
 
 // DOM Order
 const order = document.getElementById("sort-order");
 order.addEventListener('change', function (event) {
-    arrayPilots = sortData(arrayPilots, "name", event.target.value);
-    containerRoot.innerHTML = "";
-    containerRoot.appendChild(renderItems(arrayPilots));
+  arrayPilots = sortData(arrayPilots, "name", event.target.value);
+  containerRoot.innerHTML = "";
+  containerRoot.appendChild(renderItems(arrayPilots));
 });
-
-// Aquí va la estadistica
 
 // DOM limpiar (reiniciar la aplicación)
 const clearButton = document.querySelector('[data-testid="button-clear"]');
 clearButton.addEventListener('click', function () {
-    arrayPilots = data;
-    sortOption = null;
-    containerRoot.innerHTML = "";
-    containerRoot.appendChild(renderItems(arrayPilots));
+  // Restablece los filtros y ordenamientos
+  filter.value = 'Todos';
+  order.value = 'sin ordenar';
+
+  arrayPilots = data;
+  sortOption = null;
+  containerRoot.innerHTML = "";
+  containerRoot.appendChild(renderItems(arrayPilots));
+});
+
+// Agrega funcionalidad del menú
+const menu = document.querySelector('.menu');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hamburgerMenu.addEventListener('click', function () {
+  menu.classList.toggle('opened');
+});
+
+// Cerrar el menú al hacer clic en un elemento del menú (puedes personalizarlo según tu estructura de menú)
+const menuItems = document.querySelectorAll('.menu-item');
+menuItems.forEach(function (item) {
+  item.addEventListener('click', function () {
+    menu.classList.remove('opened');
+  });
 });

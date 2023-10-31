346093d327ed6c742e14ecf71be74aaf94f221b0
Rubi <rubiestram@gmail.com>
2023-10-31 07:47:55 -0600

    Agregue estilo a las cards

--- src/view.js
+++ src/view.js
@@ -1,19 +1,22 @@
 export const renderItems = (data) => {
-  const ul =  document.createElement("ul");
+  const ul = document.createElement("ul");
 
-  for(let i = 0; i < data.length; i++){
+  for (let i = 0; i < data.length; i++) {
     const element = data[i];
     const li = document.createElement("li");
-    li.innerHTML = ""; 
+    li.setAttribute("itemscope", "");
+    li.setAttribute("itemtype", "PilotsF1");
+
     li.innerHTML = `
-    <dl class="cards" itemscope itemtype="PilotsF1">
-    <img src="${element.imageUrl}"/>
-    <dd itemprop="name">${element.name}</dd>
-    <dd itemprop="placeOfBirth">${element.facts.placeOfBirth}</dd>
-    <dd itemprop="team">${element.facts.team}</dd>
-    <dt>Campeonatos:</dt><dd itemprop="championshipsWon">${element.extraInfo.championshipsWon}</dd>
-    </dl>
-  `
+      <dl class="root-list">
+        <img src="${element.imageUrl}" itemprop="image" />
+        <dd itemprop="name">${element.name}</dd>
+        <dd itemprop="placeOfBirth">${element.facts.placeOfBirth}</dd>
+        <dd itemprop="team">${element.facts.team}</dd>
+        <dt class="champ">Campeonatos:</dt><dd itemprop="championshipsWon">${element.extraInfo.championshipsWon}</dd>
+      </dl>
+    `;
+
     ul.appendChild(li);
   }
   return ul;

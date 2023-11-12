08b83b294111442dc7b5cab49e099c4abf1cb9a0
Rubi <rubiestram@gmail.com>
2023-11-11 22:57:38 -0600

    Cambios minimos a test

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,7 +1,7 @@
 import { filterData } from './dataFunctions'; 
 import { data as fakeData } from "./data.js";
 
-describe('Data Functions', () => {
+describe('filterData', () => {
   it('Filtrar por escuderia - Williams', () => {
     const filteredData = filterData(fakeData, 'Williams');
     expect(filteredData.every(driver => driver.facts.team === 'Williams')).toBe(true);

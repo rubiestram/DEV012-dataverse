ec372a9fa25bd2a62e1f33b05427786344b148f9
Rubi <rubiestram@gmail.com>
2023-11-13 11:09:27 -0600

    Agregue cambios a pruebas unitarias y package

--- test/data.js
+++ test/data.js
@@ -1,87 +1,97 @@
 export const data = [
-    {
-        "id": "alain-prost",
-        "name": "Alain Prost",
-        "shortDescription": "Piloto francés conocido como: El Profesor. Con 4 campeonatos mundiales, es una leyenda de la Fórmula 1.",
-        "description": "Apodado: El Profesor, este piloto legendario es ampliamente considerado uno de los mejores pilotos en la historia de la Fórmula 1. Con una astucia estratégica y un estilo de conducción calculado, ganó 4 campeonatos mundiales y es una verdadera leyenda del deporte. Su rivalidad con Ayrton Senna se ha convertido en un capítulo legendario en la historia del automovilismo. A lo largo de su carrera, demostró una habilidad excepcional para gestionar carreras y equipos, consolidando su estatus como una verdadera leyenda del deporte.",
-        "imageUrl": "./data/images/alain_prost.png",
-        "facts": {
-            "yearOfBirth": 1955,
-            "yearOfDeath": "Sigue vivo",
-            "placeOfBirth": "Lorette, Francia",
-            "team": "Williams"
-        },
-        "extraInfo": {
-            "podiums": 106,
-            "championshipsWon": 4
-        }
+  {
+    id: "alain-prost",
+    name: "Alain Prost",
+    shortDescription:
+      "Piloto francés conocido como: El Profesor. Con 4 campeonatos mundiales, es una leyenda de la Fórmula 1.",
+    description:
+      "Apodado: El Profesor, este piloto legendario es ampliamente considerado uno de los mejores pilotos en la historia de la Fórmula 1. Con una astucia estratégica y un estilo de conducción calculado, ganó 4 campeonatos mundiales y es una verdadera leyenda del deporte. Su rivalidad con Ayrton Senna se ha convertido en un capítulo legendario en la historia del automovilismo. A lo largo de su carrera, demostró una habilidad excepcional para gestionar carreras y equipos, consolidando su estatus como una verdadera leyenda del deporte.",
+    imageUrl: "./data/images/alain_prost.png",
+    facts: {
+      yearOfBirth: 1955,
+      yearOfDeath: "Sigue vivo",
+      placeOfBirth: "Lorette, Francia",
+      team: "Williams",
     },
-    {
-        "id": "michael-schumacher",
-        "name": "Michael Schumacher",
-        "shortDescription": "Icónico piloto alemán con 7 campeonatos mundiales y un legado duradero en la Fórmula 1.",
-        "description": "El legendario piloto alemán de Fórmula 1, es conocido por sus 7 campeonatos mundiales y su destacado paso por Ferrari, marcando una era dorada en la escudería italiana. Su regreso a la Fórmula 1 con Mercedes, aunque menos exitoso, resaltó su pasión por las carreras. Trágicamente, en 2013, sufrió un grave accidente de esquí que cambió su vida. Desde entonces, su salud es motivo de privacidad y preocupación. A pesar de las adversidades, Schumacher sigue siendo un símbolo de valentía y determinación, dejando un legado duradero como uno de los más grandes en la historia de la Fórmula 1.",
-        "imageUrl": "./data/images/michael_shumacher.png",
-        "facts": {
-            "yearOfBirth": 1969,
-            "yearOfDeath": "Sigue vivo",
-            "placeOfBirth": "Hürth-Hermülheim, Alemania",
-            "team": "Mercedes",
-        },
-        "extraInfo": {
-            "podiums": 155,
-            "championshipsWon": 7
-        }
+    extraInfo: {
+      podiums: 106,
+      championshipsWon: 4,
     },
-    {
-        "id": "lewis-hamilton",
-        "name": "Lewis Hamilton",
-        "shortDescription": "Piloto británico con múltiples campeonatos mundiales y un compromiso por la igualdad y el activismo.",
-        "description": "Este piloto británico es mucho más que un campeón de la Fórmula 1. Con una impresionante colección de campeonatos mundiales, ha dejado su marca como uno de los pilotos más exitosos de todos los tiempos. Pero su influencia trasciende las pistas, ya que se ha convertido en un defensor apasionado de la igualdad y un activista en temas sociales importantes. Su habilidad en la pista se combina con un compromiso por causas como el cambio climático y la diversidad en el automovilismo, lo que lo hace único en el mundo del deporte de motor.",
-        "imageUrl": "./data/images/lewis_hamilton.png",
-        "facts": {
-            "yearOfBirth": 1985,
-            "yearOfDeath": "Sigue vivo",
-            "placeOfBirth": "Stevenage, Reino Unido",
-            "team": "Mercedes"
-        },
-        "extraInfo": {
-            "podiums": 196,
-            "championshipsWon": 7
-        }
+  },
+  {
+    id: "michael-schumacher",
+    name: "Michael Schumacher",
+    shortDescription:
+      "Icónico piloto alemán con 7 campeonatos mundiales y un legado duradero en la Fórmula 1.",
+    description:
+      "El legendario piloto alemán de Fórmula 1, es conocido por sus 7 campeonatos mundiales y su destacado paso por Ferrari, marcando una era dorada en la escudería italiana. Su regreso a la Fórmula 1 con Mercedes, aunque menos exitoso, resaltó su pasión por las carreras. Trágicamente, en 2013, sufrió un grave accidente de esquí que cambió su vida. Desde entonces, su salud es motivo de privacidad y preocupación. A pesar de las adversidades, Schumacher sigue siendo un símbolo de valentía y determinación, dejando un legado duradero como uno de los más grandes en la historia de la Fórmula 1.",
+    imageUrl: "./data/images/michael_shumacher.png",
+    facts: {
+      yearOfBirth: 1969,
+      yearOfDeath: "Sigue vivo",
+      placeOfBirth: "Hürth-Hermülheim, Alemania",
+      team: "Mercedes",
     },
-    {
-        "id": "niki-lauda",
-        "name": "Niki Lauda",
-        "shortDescription": "Piloto austriaco con una increíble historia de superación y 3 campeonatos mundiales en la Fórmula 1.",
-        "description": "La vida y carrera de este piloto austriaco son una inspiración. Ganador de 3 campeonatos mundiales en la Fórmula 1, su historia es aún más asombrosa debido a su valentía y determinación para regresar a las carreras después de un grave accidente en Nürburgring en 1976. Su espíritu indomable y su dedicación al automovilismo lo convirtieron en una leyenda. Además, su franqueza y autenticidad en un mundo a menudo lleno de secretos lo hicieron querido por los fanáticos. Lauda dejó un legado duradero tanto en la pista como fuera de ella.",
-        "imageUrl": "./data/images/niki_lauda.png",
-        "facts": {
-            "yearOfBirth": 1949,
-            "yearOfDeath": 2019,
-            "placeOfBirth": "Viena, Austria",
-            "team": "McLaren"
-        },
-        "extraInfo": {
-            "podiums": 54,
-            "championshipsWon": 3
-        }
+    extraInfo: {
+      podiums: 155,
+      championshipsWon: 7,
     },
-    {
-        "id": "nigel-mansell",
-        "name": "Nigel Mansell",
-        "shortDescription": "Piloto británico de Fórmula 1 con un campeonato mundial y un espíritu competitivo feroz en las pistas de carreras.",
-        "description": "apasionado piloto británico de Fórmula 1, logró un campeonato mundial y compitió para escuderías notables como Williams, Ferrari y Lotus. Su estilo de conducción agresivo y valentía en las pistas lo convirtieron en un luchador incansable. Uno de los momentos icónicos de su carrera fue su victoria en el Gran Premio de Gran Bretaña de 1987 en Silverstone, donde logró una memorable victoria en casa. Mansell era admirado por su carisma y determinación en las carreras. Su legado perdura como un testimonio de su excepcional talento y su contribución al automovilismo británico.",
-        "imageUrl": "./data/images/nigel_mansell.png",
-        "facts": {
-            "yearOfBirth": 1953,
-            "yearOfDeath": "Sigue vivo",
-            "placeOfBirth": "Upton-upon-S., Reino Unido",
-            "team": "Williams"
-        },
-        "extraInfo": {
-            "podiums": 59,
-            "championshipsWon": 1
-        }
-    }
+  },
+  {
+    id: "lewis-hamilton",
+    name: "Lewis Hamilton",
+    shortDescription:
+      "Piloto británico con múltiples campeonatos mundiales y un compromiso por la igualdad y el activismo.",
+    description:
+      "Este piloto británico es mucho más que un campeón de la Fórmula 1. Con una impresionante colección de campeonatos mundiales, ha dejado su marca como uno de los pilotos más exitosos de todos los tiempos. Pero su influencia trasciende las pistas, ya que se ha convertido en un defensor apasionado de la igualdad y un activista en temas sociales importantes. Su habilidad en la pista se combina con un compromiso por causas como el cambio climático y la diversidad en el automovilismo, lo que lo hace único en el mundo del deporte de motor.",
+    imageUrl: "./data/images/lewis_hamilton.png",
+    facts: {
+      yearOfBirth: 1985,
+      yearOfDeath: "Sigue vivo",
+      placeOfBirth: "Stevenage, Reino Unido",
+      team: "Mercedes",
+    },
+    extraInfo: {
+      podiums: 196,
+      championshipsWon: 7,
+    },
+  },
+  {
+    id: "niki-lauda",
+    name: "Niki Lauda",
+    shortDescription:
+      "Piloto austriaco con una increíble historia de superación y 3 campeonatos mundiales en la Fórmula 1.",
+    description:
+      "La vida y carrera de este piloto austriaco son una inspiración. Ganador de 3 campeonatos mundiales en la Fórmula 1, su historia es aún más asombrosa debido a su valentía y determinación para regresar a las carreras después de un grave accidente en Nürburgring en 1976. Su espíritu indomable y su dedicación al automovilismo lo convirtieron en una leyenda. Además, su franqueza y autenticidad en un mundo a menudo lleno de secretos lo hicieron querido por los fanáticos. Lauda dejó un legado duradero tanto en la pista como fuera de ella.",
+    imageUrl: "./data/images/niki_lauda.png",
+    facts: {
+      yearOfBirth: 1949,
+      yearOfDeath: 2019,
+      placeOfBirth: "Viena, Austria",
+      team: "McLaren",
+    },
+    extraInfo: {
+      podiums: 54,
+      championshipsWon: 3,
+    },
+  },
+  {
+    id: "nigel-mansell",
+    name: "Nigel Mansell",
+    shortDescription:
+      "Piloto británico de Fórmula 1 con un campeonato mundial y un espíritu competitivo feroz en las pistas de carreras.",
+    description:
+      "apasionado piloto británico de Fórmula 1, logró un campeonato mundial y compitió para escuderías notables como Williams, Ferrari y Lotus. Su estilo de conducción agresivo y valentía en las pistas lo convirtieron en un luchador incansable. Uno de los momentos icónicos de su carrera fue su victoria en el Gran Premio de Gran Bretaña de 1987 en Silverstone, donde logró una memorable victoria en casa. Mansell era admirado por su carisma y determinación en las carreras. Su legado perdura como un testimonio de su excepcional talento y su contribución al automovilismo británico.",
+    imageUrl: "./data/images/nigel_mansell.png",
+    facts: {
+      yearOfBirth: 1953,
+      yearOfDeath: "Sigue vivo",
+      placeOfBirth: "Upton-upon-S., Reino Unido",
+      team: "Williams",
+    },
+    extraInfo: {
+      podiums: 59,
+      championshipsWon: 1,
+    },
+  },
 ];
--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,15 +1,115 @@
-import { filterData } from './dataFunctions'; 
-import { data as fakeData } from "./data.js";
+import { filterData, sortData } from "../src/dataFunctions.js";
+import { data as dataFake } from "./data.js";
 
-describe('filterData', () => {
-  it('Filtrar por escuderia - Williams', () => {
-    const filteredData = filterData(fakeData, 'Williams');
-    expect(filteredData.every(driver => driver.facts.team === 'Williams')).toBe(true);
+// describe("filterData", () => {
+//   it("Filtrar por escuderia - Williams", () => {
+//     const arrayEntrada = [
+//       {
+//         id: "alain-prost",
+//         name: "Alain Prost",
+//         shortDescription:
+//           "Piloto francés conocido como: El Profesor. Con 4 campeonatos mundiales, es una leyenda de la Fórmula 1.",
+//         description:
+//           "Apodado: El Profesor, este piloto legendario es ampliamente considerado uno de los mejores pilotos en la historia de la Fórmula 1. Con una astucia estratégica y un estilo de conducción calculado, ganó 4 campeonatos mundiales y es una verdadera leyenda del deporte. Su rivalidad con Ayrton Senna se ha convertido en un capítulo legendario en la historia del automovilismo. A lo largo de su carrera, demostró una habilidad excepcional para gestionar carreras y equipos, consolidando su estatus como una verdadera leyenda del deporte.",
+//         imageUrl: "./data/images/alain_prost.png",
+//         facts: {
+//           yearOfBirth: 1955,
+//           yearOfDeath: "Sigue vivo",
+//           placeOfBirth: "Lorette, Francia",
+//           team: "Williams",
+//         },
+//         extraInfo: {
+//           podiums: 106,
+//           championshipsWon: 4,
+//         },
+//       },
+//       {
+//         id: "michael-schumacher",
+//         name: "Michael Schumacher",
+//         shortDescription:
+//           "Icónico piloto alemán con 7 campeonatos mundiales y un legado duradero en la Fórmula 1.",
+//         description:
+//           "El legendario piloto alemán de Fórmula 1, es conocido por sus 7 campeonatos mundiales y su destacado paso por Ferrari, marcando una era dorada en la escudería italiana. Su regreso a la Fórmula 1 con Mercedes, aunque menos exitoso, resaltó su pasión por las carreras. Trágicamente, en 2013, sufrió un grave accidente de esquí que cambió su vida. Desde entonces, su salud es motivo de privacidad y preocupación. A pesar de las adversidades, Schumacher sigue siendo un símbolo de valentía y determinación, dejando un legado duradero como uno de los más grandes en la historia de la Fórmula 1.",
+//         imageUrl: "./data/images/michael_shumacher.png",
+//         facts: {
+//           yearOfBirth: 1969,
+//           yearOfDeath: "Sigue vivo",
+//           placeOfBirth: "Hürth-Hermülheim, Alemania",
+//           team: "Mercedes",
+//         },
+//         extraInfo: {
+//           podiums: 155,
+//           championshipsWon: 7,
+//         },
+//       },
+//     ];
+//     const resultado = [
+//       {
+//         id: "alain-prost",
+//         name: "Alain Prost",
+//         shortDescription:
+//           "Piloto francés conocido como: El Profesor. Con 4 campeonatos mundiales, es una leyenda de la Fórmula 1.",
+//         description:
+//           "Apodado: El Profesor, este piloto legendario es ampliamente considerado uno de los mejores pilotos en la historia de la Fórmula 1. Con una astucia estratégica y un estilo de conducción calculado, ganó 4 campeonatos mundiales y es una verdadera leyenda del deporte. Su rivalidad con Ayrton Senna se ha convertido en un capítulo legendario en la historia del automovilismo. A lo largo de su carrera, demostró una habilidad excepcional para gestionar carreras y equipos, consolidando su estatus como una verdadera leyenda del deporte.",
+//         imageUrl: "./data/images/alain_prost.png",
+//         facts: {
+//           yearOfBirth: 1955,
+//           yearOfDeath: "Sigue vivo",
+//           placeOfBirth: "Lorette, Francia",
+//           team: "Williams",
+//         },
+//         extraInfo: {
+//           podiums: 106,
+//           championshipsWon: 4,
+//         },
+//       },
+//     ];
+//     expect(filterData(arrayEntrada, "team", "Williams")).toEqual(resultado);
+//   });
+// });
+
+describe("filterData", () => {
+  it("debe filtrar los pilotos por escudería - Williams", () => {
+    const filteredData = filterData(dataFake, "team", "Williams");
+    expect(filteredData.length).toBe(2);
   });
 
-  it('Filtrar por escuderia - Mercedes', () => {
-    const filteredData = filterData(fakeData, 'Mercedes');
-    expect(filteredData.every(driver => driver.facts.team === 'Mercedes')).toBe(true);
+  it("debe filtrar los pilotos por escudería - Mercedes", () => {
+    const filteredData = filterData(dataFake, "team", "Mercedes");
+    expect(filteredData.length).toBe(2);
   });
 
+  it("debe filtrar los pilotos por escudería - McLaren", () => {
+    const filteredData = filterData(dataFake, "team", "McLaren");
+    expect(filteredData.length).toBe(1);
+  });
 });
+
+
+describe("sortData", () => {
+  it("debe ordenar los nombres ascendentemente", () => {
+    const fakeData = [
+      "Niki Lauda",
+      "Lewis Hamilton",
+      "Nigel Mansell",
+      "Alain Prost",
+      "Michael Schumacher",
+    ];
+
+    const sortedData = sortData([...fakeData], "name", "asc");
+
+    const resultSortData = [
+      "Alain Prost",
+      "Lewis Hamilton",
+      "Michael Schumacher",
+      "Nigel Mansell",
+      "Niki Lauda",
+    ];
+    
+    expect(sortedData).toEqual(resultSortData);
+  });
+});
+
+
+
+
